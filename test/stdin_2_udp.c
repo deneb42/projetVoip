41893c64cc6aff6bdcb30e7a8709df8b46f4f7a3
deneb <benjblois@gmail.com>
2011-11-15 11:20:13 +0100

    attempt to maek a two ways connection

--- test/stdin_2_udp.c
+++ test/stdin_2_udp.c
@@ -13,15 +13,16 @@
 
 int main (int argc, char *argv[])
 {
-	int nb_lus;
 	struct sockaddr_in adresse;
-	char buffer[LG_BUFFER];
+	char buffer[LG_BUFFER], buffer2[LG_BUFFER];
+	int nb_lus;
 	int sock = openSock(argc, argv, & adresse);
-	
+	//int sock2 = openSock(argc, argv, &adresse);
+	/*
 	if (bind(sock, (struct sockaddr *) &adresse, sizeof(struct sockaddr_in)) < 0) {
 		perror("bind");
 		exit(EXIT_FAILURE);
-	}
+	}*/
 	
 	while (1) 
 	{
@@ -35,6 +36,12 @@ int main (int argc, char *argv[])
 		}
 		sendto(sock, buffer, nb_lus, 0,
 			(struct sockaddr *) & adresse, sizeof (struct sockaddr_in));
+			
+		//if ((nb_lus = recv(sock, buffer2, LG_BUFFER, 0)) == 0)
+		//	break;
+		
+		//write(STDOUT_FILENO, buffer2, nb_lus);
+		// ne marche pas encore
 	}
 	
 	return EXIT_SUCCESS;
--- test/stdout_2_udp.c
+++ test/stdout_2_udp.c
@@ -13,29 +13,34 @@
 
 int main (int argc, char *argv[])
 {
+	int init = 1 ;
 	struct sockaddr_in adresse;
 	char buffer[LG_BUFFER];
 	int nb_lus;
 	int sock = openSock(argc, argv, &adresse);
+	//int sock2 = openSock(argc, argv, &adresse);
 	
-	if (bind(sock, (struct sockaddr *) &adresse, sizeof(struct sockaddr_in)) < 0) {
+	if (bind(sock, (struct sockaddr *) &adresse, sizeof(struct sockaddr_in)) < 0) 
+	{
 		perror("bind");
 		exit(EXIT_FAILURE);
 	}
-	
-	//setvbuf(stdout, NULL, _IONBF, 0);
 	
 	while (1) 
 	{
 		if ((nb_lus = recv(sock, buffer, LG_BUFFER, 0)) == 0)
 			break;
-			
+				
 		if (nb_lus < 0) {
 			perror("read");
 			break;
 		}
 		
 		write(STDOUT_FILENO, buffer, nb_lus);
+		
+		//sendto(sock, buffer, nb_lus, 0,
+		//	(struct sockaddr *) & adresse, sizeof (struct sockaddr_in));
+		// ne marche pas surement car on est sur la meme machine
 	}
 	
 	return EXIT_SUCCESS;
