e06d84b484a2b6345493dd31efa340b621b4be55
deneb <benjblois@gmail.com>
2011-11-26 15:23:42 +0100

    audio chanels in the two ways now

--- src/client.c
+++ src/client.c
@@ -13,9 +13,9 @@
 #define LG_BUFFER 1024
 
 
-void traitement_client(int sock, struct sockaddr_in * serveur, void* element, int size)
+void traitement_client(int sock, struct sockaddr_in * serveur, void** element, int size)
 {
-	char buffer[LG_BUFFER];
+	//char buffer[LG_BUFFER];
 	int nb_lus;
 
 	/*if ((nb_lus = read(STDIN_FILENO, buffer, LG_BUFFER)) == 0)
@@ -27,10 +27,10 @@ void traitement_client(int sock, struct sockaddr_in * serveur, void* element, in
 		return;
 	}*/
 	
-	sendto(sock, element, size, 0, (struct sockaddr *) serveur, sizeof (struct sockaddr_in));
+	sendto(sock, element[0], size, 0, (struct sockaddr *) serveur, sizeof (struct sockaddr_in));
 		
-	if ((nb_lus = recv(sock, buffer, LG_BUFFER, MSG_DONTWAIT)) == 0)
+	if ((nb_lus = recv(sock, element[1], size, MSG_DONTWAIT)) == 0)
 		return;
 	
-	write(STDOUT_FILENO, buffer, nb_lus);
+	//write(STDOUT_FILENO, buffer, nb_lus);
 }
