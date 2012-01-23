dc5c032b896c0d2719d70a8bc299b8fa186d359b
sbl <samy.ben-lemrid@u-psud.fr>
2012-01-23 15:52:19 +0100

    added an option for the port number

--- src/interface.c
+++ src/interface.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
-#include <unistd.h>
 
-void on_clicked_button(GtkWidget *pButton, gpointer data);
+
+void on_clicked_button_connection(GtkWidget *pButton, gpointer data);
+void on_clicked_button_disconnection(GtkWidget *pButton, gpointer data);
 
 int main(int argc, char **argv)
 {
@@ -12,30 +13,38 @@ int main(int argc, char **argv)
     	GtkWidget *pFrame;
     	GtkWidget *pVBoxFrame;
     	GtkWidget *pSeparator;
-    	GtkWidget *pEntry1;
-	GtkWidget *pEntry2;
-    	GtkWidget *pButton;
+    	GtkWidget *pEntry;
+	GtkWidget *pButton;
 	GtkWidget *pLabel;
+	GtkWidget *pImage;
 
     	gtk_init(&argc, &argv);
+
+	/*Chargement des parametres d arriere plan */
+	gtk_rc_parse("./window.rc");
 
     	/* Creation de la fenetre */
 	pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	/* On ajoute un espace de 5 sur les bords de la fenetre */
-    	gtk_container_set_border_width(GTK_CONTAINER(pWindow), 5);
+    	gtk_container_set_border_width(GTK_CONTAINER(pWindow), 3);
 	/* Definition de la position */
  	gtk_window_set_position(GTK_WINDOW(pWindow), GTK_WIN_POS_CENTER);
 	/* Titre de la fenetre */
 	gtk_window_set_title(GTK_WINDOW(pWindow), "VoIP");
 	/* Definition de la taille de la fenetre */
 	gtk_window_set_default_size(GTK_WINDOW(pWindow), 320, 240);
-
+	
 	/* Connexion du signal "destroy" de la fenetre */
 	/* On appelle directement la fonction de sortie de boucle */
     	g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     	/* Creation de la GtkBox verticale */
     	pVBox = gtk_vbox_new(TRUE, 0);
+
+	/* Chargement d'une image a partir d'un fichier */
+    	pImage = gtk_image_new_from_file("./voip.png");
+    	gtk_box_pack_start(GTK_BOX(pVBox), pImage, FALSE, FALSE, 0);
+
     	/* Ajout de la GtkVBox dans la fenetre */
     	gtk_container_add(GTK_CONTAINER(pWindow), pVBox);
 
@@ -50,23 +59,33 @@ int main(int argc, char **argv)
     	/* Creation et insertion des elements contenus dans le premier GtkFrame */
     	pLabel = gtk_label_new("Adresse :");
     	gtk_box_pack_start(GTK_BOX(pVBoxFrame), pLabel, TRUE, TRUE, 0);
-    	pEntry1 = gtk_entry_new();
-    	gtk_box_pack_start(GTK_BOX(pVBoxFrame), pEntry1, TRUE, FALSE, 0);
+    	pEntry = gtk_entry_new();
+    	gtk_box_pack_start(GTK_BOX(pVBoxFrame), pEntry, TRUE, FALSE, 0);
     	pLabel = gtk_label_new("N° de port :");
     	gtk_box_pack_start(GTK_BOX(pVBoxFrame), pLabel, TRUE, FALSE, 0);
-    	pEntry2 = gtk_entry_new();
-    	gtk_box_pack_start(GTK_BOX(pVBoxFrame), pEntry2, TRUE, FALSE, 0);
+    	pEntry = gtk_entry_new();
+    	gtk_box_pack_start(GTK_BOX(pVBoxFrame), pEntry, TRUE, FALSE, 0);
 
     	/* Creation d un GtkHSeparator */
     	pSeparator = gtk_hseparator_new();
     	gtk_box_pack_start(GTK_BOX(pVBoxFrame), pSeparator, TRUE, FALSE, 0);
-	
-	/* Creation du bouton avec un label*/
-    	pButton = gtk_button_new_with_label("Connexion");
-    	gtk_box_pack_start(GTK_BOX(pVBox), pButton, TRUE, FALSE, 0);
 
+	pImage = gtk_image_new_from_file("./phone.gif");
+    	gtk_box_pack_start(GTK_BOX(pVBox), pImage, TRUE, TRUE, 0);
+	
+	/* Creation du bouton avec un label pour se connecter */
+    	pButton = gtk_button_new_with_label("Connexion");
+    	gtk_box_pack_start(GTK_BOX(pVBox), pButton, FALSE, FALSE, 0);
+	
     	/* Connexion du signal "clicked" du GtkButton */
-    	g_signal_connect(G_OBJECT(pButton), "clicked", G_CALLBACK(on_clicked_button), (GtkWidget*) pEntry1);
+    	g_signal_connect(G_OBJECT(pButton), "clicked", G_CALLBACK(on_clicked_button_connection), (GtkWidget*) pVBoxFrame);
+
+	/* Creation du bouton avec un label pour se deconnecter */
+	pButton = gtk_button_new_with_label("Déconnexion");
+    	gtk_box_pack_start(GTK_BOX(pVBox), pButton, FALSE, FALSE, 0);
+
+	/* Connexion du signal "clicked" du GtkButton */
+    	/*g_signal_connect(G_OBJECT(pButton), "clicked", G_CALLBACK(on_clicked_button_disconnection), (GtkWidget*) pWindow);*/
 	
 	/* Affichage de la fenetre et de tout ce qu'elle contient */
     	gtk_widget_show_all(pWindow);
@@ -77,13 +96,49 @@ int main(int argc, char **argv)
     	return EXIT_SUCCESS;
 }
 
-void on_clicked_button(GtkWidget *pButton, gpointer data)
+void on_clicked_button_connection(GtkWidget *pButton, gpointer data)
 {
-	const gchar *adresse;
+	GtkWidget *pTempEntry;
+    	GList *pList;
+	const gchar *adress;
+	const gchar *port;
+	
+    	/* Recuperation de la liste des elements que contient la GtkVBox */
+    	pList = gtk_container_get_children(GTK_CONTAINER((GtkWidget*)data));
+
+    	/* Le premier element est le GtkLabel "Adresse:" */
+    	/* Passage a l element suivant : le GtkEntry */
+    	pList = g_list_next(pList);
+    	pTempEntry = GTK_WIDGET(pList->data);
+
+    	/* Recuperation du texte contenu dans le 1er GtkEntry */
+    	adress = gtk_entry_get_text(GTK_ENTRY(pTempEntry));
+
+	/* Passage a l element suivant : le GtkLabel "N° de port :" */
+    	pList = g_list_next(pList);
+
+	/* Passage a l element suivant : le GtkEntry */
+    	pList = g_list_next(pList);
+	pTempEntry = GTK_WIDGET(pList->data);
+
+	/* Recuperation du texte contenu dans le 2e GtkEntry */
+    	port = gtk_entry_get_text(GTK_ENTRY(pTempEntry));
+
 	printf("coucou\n");
-	/*const char *port;*/
-	/* Recuperation du texte contenu dans le GtkEntry */
-    	adresse = gtk_entry_get_text(GTK_ENTRY((GtkWidget*)data));
-    launch(adresse);
+
+	/*Appel de la fonction principale */
+    	launch(adress, port);
+
+	/* Liberation de la memoire utilisee par la liste */
+    	g_list_free(pList);
+}
+
+void on_clicked_button_disconnection(GtkWidget *pButton, gpointer data)
+{	
+	/*Chargement des parametres d arriere plan */
+	/*gtk_rc_parse("./window.rc");*/
+	
+	/* Affichage de la fenetre et de tout ce qu'elle contient */
+    	/*gtk_widget_show_all(GTK_WIDGET((GtkWidget*)data));*/
 }
 
